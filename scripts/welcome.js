d78e6069e0129c8439ab8b0d81cf63f1a3a81613
Giuliano1911 <giulianotorrisi1911@yahoo.com>
2024-10-21 23:51:27 +0200

    Fixes input checkbox welcome page

--- scripts/welcome.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('proceed').addEventListener('click', function (e) {
-  e.preventDefault()
-})
-document.getElementById('proceed').addEventListener('click', function () {
-  window.location.href = 'benchmark.html'
-})
