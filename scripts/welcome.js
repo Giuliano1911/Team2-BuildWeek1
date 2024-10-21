eaa31ddeeb93e63495e976847268e36a64e68108
Giuliano1911 <giulianotorrisi1911@yahoo.com>
2024-10-21 17:45:25 +0200

    Tries to fix the checkbox button

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,6 +1,6 @@
-document.getElementById("proceed").addEventListener("click", function (e) {
+document.getElementById('proceed').addEventListener('click', function (e) {
   e.preventDefault()
 })
-document.getElementById("proceed").addEventListener("click", function () {
-  window.location.href = "benchmark.html"
+document.getElementById('proceed').addEventListener('click', function () {
+  window.location.href = 'benchmark.html'
 })
