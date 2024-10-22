9038fe4ea3896c05b2e552587c1fb2cb9171cb9b
Giovanni Mauro <giovannimauro95@hotmail.it>
2024-10-22 12:09:08 +0200

    Finished stars

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,15 +1,32 @@
 const stars = document.querySelectorAll(".star")
 
-stars.forEach((star, index) => {
-  star.addEventListener("mouseover", () => {
-    for (let i = 0; i <= index; i++) {
-      stars[i].src = "./assets/img/star.svg"
-    }
-  })
+let isClicked = false
 
-  star.addEventListener("mouseout", () => {
-    for (let i = 0; i < stars.length; i++) {
-      stars[i].src = "./assets/img/unselectedstar.svg"
+stars.forEach((star, index) => {
+  star.onmouseover = () => {
+    if (!isClicked) {
+      for (let i = 0; i <= index; i++) {
+        stars[i].src = "./assets/img/star.svg"
+      }
     }
-  })
+  }
+
+  star.onmouseout = () => {
+    if (!isClicked) {
+      for (let i = 0; i < stars.length; i++) {
+        stars[i].src = "./assets/img/unselectedstar.svg"
+      }
+    }
+  }
+
+  star.onclick = () => {
+    selectedIndex = index
+    isClicked = true
+    for (let i = 0; i < stars.length; i++) {
+      stars[i].src =
+        i <= selectedIndex
+          ? "./assets/img/star.svg"
+          : "./assets/img/unselectedstar.svg"
+    }
+  }
 })
