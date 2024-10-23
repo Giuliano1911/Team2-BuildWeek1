f73524ac34d204f0984efeb8a2690a6c86a7db03
Giuliano1911 <giulianotorrisi1911@yahoo.com>
2024-10-23 11:08:16 +0200

    final version

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -274,24 +274,24 @@ const arrayChoose = (diff) => {
 // randomizedAnswers è l'array delle risposte randomizzate
 const questionGenerator = (array, numb, i, randomizedAnswers) => {
   const main = document.getElementsByTagName('main')[0]
-// crea il paragrafo per la domanda
+  // crea il paragrafo per la domanda
   const newP = document.createElement('p')
   newP.innerText = array[i].question
   newP.classList.add('p')
   main.appendChild(newP)
-// crea il div per le risposte
+  // crea il div per le risposte
   const answerContainer = document.createElement('div')
   answerContainer.style.display = 'flex'
   answerContainer.style.justifyContent = 'center'
   answerContainer.style.flexWrap = 'wrap'
-// crea i bottoni per le risposte randomizzate
-  answerContainer.style.display = 'grid';
-  answerContainer.style.gridTemplateColumns = 'repeat(2, 1fr)';
-  answerContainer.style.gridGap = '10px';
-  answerContainer.style.maxWidth = '400px';
-  answerContainer.style.margin = '0 auto';
-//funzione per creare i bottoni per le risposte randomizzate
-  randomizedAnswers.forEach(answer => {
+  // crea i bottoni per le risposte randomizzate
+  answerContainer.style.display = 'grid'
+  answerContainer.style.gridTemplateColumns = 'repeat(2, 1fr)'
+  answerContainer.style.gap = '6em'
+  answerContainer.style.maxWidth = '800px'
+  answerContainer.style.margin = '0 auto'
+  //funzione per creare i bottoni per le risposte randomizzate
+  randomizedAnswers.forEach((answer) => {
     const button = document.createElement('button')
     button.classList.add('radio-label', 'button')
     button.innerText = answer
@@ -302,15 +302,14 @@ const questionGenerator = (array, numb, i, randomizedAnswers) => {
     button.style.fontSize = '18px'
     button.style.cursor = 'pointer'
     button.style.transition = 'all 0.3s ease'
-  
-    
+
     answerContainer.appendChild(button)
   })
 
   main.appendChild(answerContainer)
-// crea il footer per il numero della domanda
-// i è il numero della domanda corrente
-// numb è il numero di domande
+  // crea il footer per il numero della domanda
+  // i è il numero della domanda corrente
+  // numb è il numero di domande
   const footer = document.getElementsByTagName('footer')[0]
   footer.innerHTML = '' // Pulisce il contenuto esistente del footer
   const newH4 = document.createElement('h4')
@@ -347,167 +346,161 @@ form.addEventListener('submit', function (e) {
   // countdownTimer serve per contare il tempo
   // correctAnswers serve per contare le risposte corrette
   // wrongAnswers serve per contare le risposte sbagliate
-  let currentQuestion = 0;
-  let questions = [];
-  let userAnswers = [];
-  let timerElement;
-  let countdownTimer;
-  let correctAnswers = 0;
-  let wrongAnswers = 0;
-  
+  let currentQuestion = 0
+  let questions = []
+  let userAnswers = []
+  let timerElement
+  let countdownTimer
+  let correctAnswers = 0
+  let wrongAnswers = 0
+
   // Funzione per estrarre domande casuali
   // allQuestions è l'array di domande
   // numberToExtract è il numero di domande da estrarre
   const extractRandomQuestions = (allQuestions, numberToExtract) => {
-    const shuffled = [...allQuestions].sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, numberToExtract);
-  }; console.log(extractRandomQuestions(arrayChoose(difficultyValue), numberValue));
+    const shuffled = [...allQuestions].sort(() => 0.5 - Math.random())
+    return shuffled.slice(0, numberToExtract)
+  }
+  console.log(extractRandomQuestions(arrayChoose(difficultyValue), numberValue))
 
   // Funzione per randomizzare la posizione delle risposte
   // question è la domanda corrente
   // sort serve per randomizzare le risposte
   // ... serve per estrarre le risposte corrette e quelle sbagliate
   const randomizeAnswers = (question) => {
-    const allAnswers = [question.correct_answer, ...question.incorrect_answers];
-    return allAnswers.sort(() => 0.5 - Math.random());
-  };
+    const allAnswers = [question.correct_answer, ...question.incorrect_answers]
+    return allAnswers.sort(() => 0.5 - Math.random())
+  }
 
   // Funzione per mostrare la prossima domanda
   const showNextQuestion = () => {
     if (currentQuestion < questions.length) {
-      clearMainContent();
-      const randomizedAnswers = randomizeAnswers(questions[currentQuestion]);
-      questionGenerator(questions, questions.length, currentQuestion, randomizedAnswers);
-      startTimer();
-      addAnswerListeners(randomizedAnswers);
+      clearMainContent()
+      const randomizedAnswers = randomizeAnswers(questions[currentQuestion])
+      questionGenerator(
+        questions,
+        questions.length,
+        currentQuestion,
+        randomizedAnswers
+      )
+      startTimer()
+      addAnswerListeners(randomizedAnswers)
     } else {
-      console.log("Quiz completato!");
-      showResults();
+      console.log('Quiz completato!')
+      showResults()
     }
-  };
+  }
 
   // Funzione per pulire il contenuto principale
   const clearMainContent = () => {
-    const main = document.querySelector('main');
-    main.innerHTML = '';
-  };
+    const main = document.querySelector('main')
+    main.innerHTML = ''
+  }
 
   // Funzione per creare l'elemento del timer
   const createTimerElement = () => {
-    timerElement = document.createElement('div');
-    timerElement.id = 'timer';
-    document.body.appendChild(timerElement);
-  };
+    timerElement = document.createElement('div')
+    timerElement.id = 'timer'
+    document.body.appendChild(timerElement)
+  }
 
   // Funzione per avviare il timer
   const startTimer = () => {
     if (countdownTimer) {
-      clearInterval(countdownTimer);
+      clearInterval(countdownTimer)
     }
-    let timeLeft = 60;
-    timerElement.classList.remove('time-up');
+    let timeLeft = 60
+    timerElement.classList.remove('time-up')
     countdownTimer = setInterval(() => {
       // se il tempo è finito, mostra il messaggio e passa alla prossima domanda
       if (timeLeft <= 0) {
-        clearInterval(countdownTimer);
-        timerElement.textContent = "0";
-        showTimeUpMessage();
+        clearInterval(countdownTimer)
+        timerElement.textContent = '0'
+        showTimeUpMessage()
       } else {
         // se il tempo non è finito, mostra il tempo rimanente
-        timerElement.textContent = `${timeLeft}`;
-        const progress = (60 - timeLeft) / 60 * 360;
-        timerElement.style.setProperty('--progress', `${progress}deg`);
-        timeLeft--;
+        timerElement.textContent = `${timeLeft}`
+        const progress = ((60 - timeLeft) / 60) * 360
+        timerElement.style.setProperty('--progress', `${progress}deg`)
+        timeLeft--
       }
-    }, 1000);
-  };
+    }, 1000)
+  }
 
   // Funzione per mostrare il messaggio di tempo scaduto
   const showTimeUpMessage = () => {
-    const timeUpDiv = document.createElement('div');
-    timeUpDiv.id = 'time-up-message';
-    timeUpDiv.textContent = 'Tempo scaduto!';
-    document.body.appendChild(timeUpDiv);
+    const timeUpDiv = document.createElement('div')
+    timeUpDiv.id = 'time-up-message'
+    timeUpDiv.textContent = 'Tempo scaduto!'
+    document.body.appendChild(timeUpDiv)
 
     setTimeout(() => {
-      document.body.removeChild(timeUpDiv);
-      moveToNextQuestion();
-    }, 2000); // passa alla prossima domanda dopo 2 secondi
-  };
+      document.body.removeChild(timeUpDiv)
+      moveToNextQuestion()
+    }, 2000) // passa alla prossima domanda dopo 2 secondi
+  }
 
   // Funzione per aggiungere i listener alle risposte
   const addAnswerListeners = (randomizedAnswers) => {
-    const answerButtons = document.querySelectorAll('.radio-label');
+    const answerButtons = document.querySelectorAll('.radio-label')
     answerButtons.forEach((button, index) => {
       button.addEventListener('click', () => {
         if (userAnswers[currentQuestion] === undefined) {
-          userAnswers[currentQuestion] = randomizedAnswers[index];
-          if (randomizedAnswers[index] === questions[currentQuestion].correct_answer) {
-            correctAnswers++;
+          userAnswers[currentQuestion] = randomizedAnswers[index]
+          if (
+            randomizedAnswers[index] ===
+            questions[currentQuestion].correct_answer
+          ) {
+            correctAnswers++
             button.style.backgroundColor = '#4CAF50' // Verde per risposta corretta
           } else {
-            wrongAnswers++;
+            wrongAnswers++
             button.style.backgroundColor = '#F44336' // Rosso per risposta sbagliata
             // Evidenzia la risposta corretta
             answerButtons.forEach((btn, idx) => {
-              if (randomizedAnswers[idx] === questions[currentQuestion].correct_answer) {
+              if (
+                randomizedAnswers[idx] ===
+                questions[currentQuestion].correct_answer
+              ) {
                 btn.style.backgroundColor = '#4CAF50'
               }
             })
           }
           button.style.color = 'white'
-          
+
           // Disabilita tutti i bottoni dopo la risposta
-          answerButtons.forEach(btn => btn.disabled = true)
-          
-          clearInterval(countdownTimer);
-          setTimeout(moveToNextQuestion, 2000); // Passa alla prossima domanda dopo 2 secondi
+          answerButtons.forEach((btn) => (btn.disabled = true))
+
+          clearInterval(countdownTimer)
+          setTimeout(moveToNextQuestion, 2000) // Passa alla prossima domanda dopo 2 secondi
         }
-      });
-    });
-  };
+      })
+    })
+  }
 
   // Funzione per passare alla prossima domanda
   const moveToNextQuestion = () => {
-    currentQuestion++;
+    currentQuestion++
     if (currentQuestion < questions.length) {
       setTimeout(() => {
-        showNextQuestion();
-      }, 2000);
+        showNextQuestion()
+      }, 2000)
     } else {
-      showResults();
+      showResults()
     }
-  };
+  }
 
   // Funzione per mostrare i risultati finali
   const showResults = () => {
-    localStorage.setItem('correctAnswers', correctAnswers);
-    localStorage.setItem('wrongAnswers', wrongAnswers);
-    localStorage.setItem('totalQuestions', questions.length);
-    window.location.href = 'result.html';
-  };
+    localStorage.setItem('correctAnswers', correctAnswers)
+    localStorage.setItem('wrongAnswers', wrongAnswers)
+    localStorage.setItem('totalQuestions', questions.length)
+    window.location.href = 'result.html'
+  }
 
   // Inizializzazione del quiz
-  questions = extractRandomQuestions(arrayChoose(difficultyValue), numberValue);
-  userAnswers = new Array(numberValue).fill(undefined);
-  createTimerElement();
-  showNextQuestion();
+  questions = extractRandomQuestions(arrayChoose(difficultyValue), numberValue)
+  userAnswers = new Array(numberValue).fill(undefined)
+  createTimerElement()
+  showNextQuestion()
 })
-
-
-// const shuffle = (array) => {
-//   for (let i = array.length - 1; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1))
-//     ;[array[i], array[j]] = [array[j], array[i]]
-//   }
-// }
-
-// const n = array[Math.floor(Math.random() * array.length)]
-// array = array.filter(function (item) {
-//   return item !== n
-// 
-
-
-
-
-
